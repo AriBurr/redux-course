f8620fae25822157c104c0c3a5caa2fc45b2361d
Ariel Burriss <ariburr@gmail.com>
2018-04-01 12:23:53 -0600

    integrate store with router

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,7 +1,7 @@
 // a reducer takes in two things, 1) the action 2) copy of current state
 
 function comments(state = [], action) {
-
+  return state;
 }
 
 export default comments;
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,7 +1,7 @@
 // a reducer takes in two things, 1) the action 2) copy of current state
 
 function posts(state = [], action) {
-
+  return state;
 }
 
 export default posts;
